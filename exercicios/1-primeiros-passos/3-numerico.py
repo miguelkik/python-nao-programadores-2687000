a13a1fe97d6c1b9a0d761414779fdcc59ddf64c6
miguelkik <miguelaparecidoribeiro06@gmail.com>
2026-04-14 12:40:44 +0000

    feito numericos

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -1,6 +1,13 @@
 data_nascimento = '05-10-1976'
 idade_numerica = 46
 altura = 1.74
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
+
+soma = data_nascimento * 3
+print (soma)
+print(f"Hoje paloma estudou {3 + 1} horas em casa.")
 
 # Descubra o tipo de dado de cada variável acima
 
